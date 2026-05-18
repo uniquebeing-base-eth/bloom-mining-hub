c132537da2a804680bba8ddae6022edec9d32966
Uniquebeing <beingunique049@gmail.com>
2026-05-18 18:16:13 +0100

    Update wagmi.ts

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,14 +1,10 @@
 import { http, createConfig } from 'wagmi';
 import { base } from 'wagmi/chains';
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector';
-import { MetaMaskConnector } from '@wagmi/connectors/metaMask';
 
 export const wagmiConfig = createConfig({
   chains: [base],
-  connectors: [
-    miniAppConnector(),
-    new MetaMaskConnector({ chains: [base] }),
-  ],
+  connectors: [miniAppConnector()],
   transports: {
     [base.id]: http(),
   },
